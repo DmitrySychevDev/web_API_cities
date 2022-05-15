777ab4afc3edc44fbc870ac313180dbeb48c2a99
Dmitry Sychev <dimtrisythev@gmail.com>
2022-05-15 21:30:50 +0600

    Добавлен вывод городов на карту

--- web_API/Program.cs
+++ web_API/Program.cs
@@ -10,10 +10,10 @@ builder.Services.AddDbContext<CitiesContext>(options => options.UseSqlServer(con
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddRazorPages();// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,9 +22,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseRouting();
+app.UseCors(builder => builder.AllowAnyOrigin());
+app.UseEndpoints(endpoints => {
+    endpoints.MapControllers();
+});
 app.UseHttpsRedirection();
-
+app.UseDefaultFiles();
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ web_API/wwwroot/addMap.js
@@ -0,0 +1,51 @@
+﻿
+let mapElem;
+function initMAp()
+{
+    google.maps.visualRefresh=true;
+    const Moscow = new google.maps.LatLng(55.752622, 37.617567);
+    const mapOptions = {
+        zoom: 5,
+        center: Moscow,
+        mapTypeId: google.maps.MapTypeId.G_NORMAL_MAP
+    };
+    const elem=document.getElementById("convas");
+    mapElem=new google.maps.Map(elem,mapOptions);
+    const LatLng=new google.maps.LatLng(55.752622, 37.617567);
+    new google.maps.Marker({
+        position: LatLng,
+        map: mapElem,
+    });
+
+}
+let getRussianMap=async ()=>{
+
+    let response = await fetch("https://localhost:5001/api/Cities");
+	console.log(response);
+    if (response.ok) { // если HTTP-статус в диапазоне 200-299
+        // получаем тело ответа (см. про этот метод ниже)
+        let json = await response.json();
+        json.forEach((city)=>{
+            const cityLocation = new google.maps.LatLng(city.coords.lat, city.coords.lon);
+
+            let marker = new google.maps.Marker({
+                position: cityLocation,
+                map: mapElem,
+                title: city.name
+            });
+            const info=`<div><h2>${city.name}</h2><div><h4>Население:${city.population}</h4></div><div><h4>Субъект:${city.subject}</h4><div><h4>Район:${city.district}</h4></div></div>`
+            const infowindow = new google.maps.InfoWindow({
+                content:info,
+            });
+
+            google.maps.event.addListener(marker, 'click',function clickEvent(){
+    infowindow.open(mapElem,marker)
+});
+            marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png');
+        });
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+    }
+}
+initMAp();
+getRussianMap();
